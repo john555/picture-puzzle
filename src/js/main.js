5d3a4155422a1fedc9f287995e6ec710206811e8
John O. Paul <oyomthojohnpaul@gmail.com>
2018-08-11 18:28:42 +0300

    Implement game UI

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,62 @@
 'use strict';
 
-const game = new PicturePuzzle({
-  tileSize: 150,
-  columns: 5,
-  rows: 4,
+const select = function(id) {
+  return document.getElementById(id);
+};
+
+const gameContainer = select('gameContainer');
+const startButton = select('startButton');
+const minute = select('minute');
+const seconds = select('seconds');
+
+let bestTime = 0;
+
+const { width } = gameContainer.getBoundingClientRect();
+const columns = 4;
+const rows = 4;
+const tileSize = Math.round(width / columns);
+
+const gameOptions = {
+  tileSize,
+  columns,
+  rows,
+  // difficulty: 0.08,
   imageUrl: 'https://images.pexels.com/photos/1307662/pexels-photo-1307662.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+};
+
+const puzzle = new PicturePuzzle(gameOptions);
+
+startButton.addEventListener('click', () => {
+  puzzle.start();
+  startButton.style.visibility = 'hidden';
 });
 
-document.getElementById('game')
-  .appendChild(game.stage);
+function formatTime(seconds) {
+  let m = Math.floor(seconds / 60);
+  let s = seconds % 60;
+
+  m = (m < 10) ? `0${m}` : m;
+  s = (s < 10) ? `0${s}` : s;
+  return {
+    minutes: m,
+    seconds: s,
+  };
+}
+
+puzzle.onTimeUpdate(function(event) {
+  const time = formatTime(event.time);
+  minute.innerText = time.minutes;
+  seconds.innerText = time.seconds;
+  console.log('-->', event.time);
+});
+
+puzzle.onSolve(function(event) {
+  console.log(event.time);
+  startButton.style.visibility = 'visible';
+  if (event.time < bestTime) {
+    bestTime = event.time;
+  } 
+  
+});
+
+gameContainer.appendChild(puzzle.stage);
